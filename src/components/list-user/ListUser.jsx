9eee7693aac975ebc4724e51632abbd8de72c034
RomanSeveryn <romaseverin50@gmail.com>
2021-10-30 00:07:56 +0300

    add component ListUser

---
+++ src/components/list-user/ListUser.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+import UserCard from '../user-card/UserCard';
+import  './style.css'
+
+
+const ListUser = () => {
+
+  const [users, setUsers] = React.useState([])
+
+
+  const response = () => {
+   axios
+  .get(
+    `https://jsonplaceholder.typicode.com/users`
+  )
+  .then(res => {
+    const br = res.data
+    setUsers(br)
+  })
+  }
+
+  React.useEffect(() => {
+    response()
+  }, [])
+
+  
+  console.log('users', users)
+
+  const newUsers = {...users}
+
+  console.log('newUsers', newUsers)
+
+  return (
+    <div className='listContainer'>
+      {users.map(user => (
+        <UserCard name={user.name} surName={user.username} phrase={user.company.catchPhrase}/>
+      ))}
+    </div>
+  );
+}
+
+export default ListUser;
