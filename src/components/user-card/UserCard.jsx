d0192045d3754e2ea59726994fda68b18add5f42
RomanSeveryn <romaseverin50@gmail.com>
2021-10-30 00:02:11 +0300

    fix style in UserCard

--- src/components/user-card/UserCard.jsx
+++ src/components/user-card/UserCard.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import photoUser  from '../assets/image/photo.jpg'
-import headerCard  from '../assets/image/background_user_card.svg'
-import  './style.css'
+import photoUser from '../assets/image/photo.jpg'
+import headerCard from '../assets/image/background_user_card.svg'
+import './style.css'
 
 const UserCard = props => {
   return (
     <div className='container'>
-      <img className='header' src={headerCard}/>
+        <img className='header' src={headerCard} />
       <div className=''>
-        <img className='img' src={photoUser} alt='user image'/>
+        <img className='img' src={photoUser} alt='user image' />
       </div>
       <div className='name'>
-        <h2 className=''>{props.name}</h2>
-        <h2 className=''>{props.surName}</h2>
+        <h4 className=''>{props.name}</h4>
+        <h4 className='surname'>{props.surName}</h4>
       </div>
-      <p className=''>{props.phrase}</p>
+      <p className='phrase'>{props.phrase}</p>
     </div>
   )
 }
