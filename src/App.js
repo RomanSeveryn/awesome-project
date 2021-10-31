2d29cda9cfa49ae9dbf9bed1b8cfa51a583c984a
RomanSeveryn <romaseverin50@gmail.com>
2021-10-31 21:26:47 +0200

    add routing in app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import UserCard from './components/user-card/UserCard';
-import ListUser from './components/list-user/ListUser';
-import MainPage from './pages/main-page/MainPage';
-import './App.css';
+import UserCard from './components/user-card/UserCard'
+import ListUser from './components/list-user/ListUser'
+import MainPage from './pages/main-page/MainPage'
+import FriendsPage from './pages/friends-page/FriendsPage'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import './App.css'
+import Header from './components/header/Header'
 
-function App() {
+function App () {
   return (
-    <div className="app-container">
-      <header className="">
-        <MainPage/>
-      </header>
+    <div className='app-container'>
+      
+
+      <BrowserRouter>
+      <Header/>
+        <Switch>
+          <Route exact path='/'>
+            <MainPage/>
+          </Route>
+          <Route path='/mynetwork'>
+            <FriendsPage />
+          </Route>
+          <Route></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
