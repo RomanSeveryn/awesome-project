665d27e85fa1cb2c6338194abb54b96e7a294abc
RomanSeveryn <romaseverin50@gmail.com>
2021-10-31 23:14:11 +0200

    add NavLink and update input

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import linkedinIcon from '../assets/image/linkedin.png'
 import homeIcon from '../assets/image/home.png'
 import friendsIcon from '../assets/image/friends.png'
@@ -15,51 +15,56 @@ const Header = () => {
       <header className='header'>
         <div className='wrapper-header'>
           <div className='container-header-input'>
-            <Link>
-              <img src={linkedinIcon} />
-            </Link>
-            <input />
+            <div className='link-linkedin'>
+              <Link>
+                <img src={linkedinIcon} />
+              </Link>
+            </div>
+
+            <div className='input-search'>
+              <input placeholder='Поиск' className='input-header'/>
+            </div>
           </div>
           <ul className='nav-list'>
             <li className='header-link'>
-              <Link to='/'>
+              <NavLink exact to='/' activeClassName={'active'}>
                 <div className='wrapper-link'>
-                  <img className='image-header black' src={homeIcon} />
+                  <img className='image-header' src={homeIcon} />
                   <span className='name-link'>Главная</span>
                 </div>
-              </Link>
+              </NavLink>
             </li>
             <li className='header-link'>
-              <Link to='/mynetwork'>
+              <NavLink to='/mynetwork' activeClassName={'active'}>
                 <div className='wrapper-link'>
                   <img className='image-header' src={friendsIcon} />
                   <span className='name-link'>Сеть</span>
                 </div>
-              </Link>
+              </NavLink>
             </li>
             <li className='header-link'>
-              <Link to='/'>
+              <NavLink to='/vacancies' activeClassName={'active'}>
                 <div className='wrapper-link'>
                   <img className='image-header' src={vacanciesIcon} />
                   <span className='name-link'>Вакансии</span>
                 </div>
-              </Link>
+              </NavLink>
             </li>
             <li className='header-link'>
-              <Link to='/'>
+              <NavLink to='/messages' activeClassName={'active'}>
                 <div className='wrapper-link'>
                   <img className='image-header' src={messagesIcon} />
                   <span className='name-link'>Сообщения</span>
                 </div>
-              </Link>
+              </NavLink>
             </li>
             <li className='header-link'>
-              <Link to='/'>
+              <NavLink to='/notifications' activeClassName={'active'}>
                 <div className='wrapper-link'>
                   <img className='image-header' src={notifitationsIcon} />
                   <span className='name-link'>Уведомления</span>
                 </div>
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </div>
