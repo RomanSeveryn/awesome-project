3311925d0849ea4bd72033b22e58bf503a7c1c08
RomanSeveryn <romaseverin50@gmail.com>
2021-11-02 22:07:55 +0200

    render UserCard

--- src/components/list-user/ListUser.jsx
+++ src/components/list-user/ListUser.jsx
@@ -8,38 +8,16 @@ import { loadUsers } from '../../store/users/action';
 
 const ListUser = () => {
   const users = useSelector(state => state.users)
-  console.log('users', users);
-  // const [users, setUsers] = React.useState([])
   const dispatch = useDispatch()
-
-  // const response = () => {
-  //  axios
-  // .get(
-  //   `https://jsonplaceholder.typicode.com/users`
-  // )
-  // .then(res => {
-  //   const br = res.data
-  //   setUsers(br)
-  // })
-  // }
- 
-  console.log('loadUsers', loadUsers())
+  const newUsers = {...users}
 
   React.useEffect(() => {
-    // response()
       dispatch(loadUsers())
   }, [])
 
-  
-  // console.log('users', users)
-
-  // const newUsers = {...users}
-
-  // console.log('newUsers', newUsers)
-
   return (
     <div className='listContainer'>
-      {users.map(user => (
+      {newUsers.users.map(user => (
         <UserCard name={user.name} surName={user.username} key={user.id} phrase={user.company.catchPhrase}/>
       ))}
     </div>
