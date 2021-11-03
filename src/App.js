7af667955a24e1c23cf0d4e4fd79e2db519a1bbb
RomanSeveryn <romaseverin50@gmail.com>
2021-11-04 00:08:33 +0200

    add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import UserCard from './components/user-card/UserCard'
-import ListUser from './components/list-user/ListUser'
 import MainPage from './pages/main-page/MainPage'
 import FriendsPage from './pages/friends-page/FriendsPage'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import './App.css'
 import Header from './components/header/Header'
+import './App.css'
+import React from 'react'
+const FriendsPage1 = React.lazy(() => import('./pages/friends-page/FriendsPage'))
 
 function App () {
   return (
-    <div className='app-container'>
-      
-
       <BrowserRouter>
-      <Header/>
+        <Header />
+        <React.Suspense fallback={<div>Загрузка...</div>}>
         <Switch>
-          <Route exact path='/'>
-            <MainPage/>
-          </Route>
-          <Route path='/mynetwork'>
-            <FriendsPage />
-          </Route>
+          <Route exact path='/' component={MainPage}/>
+          <Route path='/mynetwork' component={FriendsPage1}/>
           <Route></Route>
         </Switch>
+        </React.Suspense>
       </BrowserRouter>
-    </div>
   )
 }
 
