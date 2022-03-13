b294aa5bf27a9ff295174eb9066f2fa6eb18340d
Roman Severyn <romaseverin50@gmail.com>
2022-03-13 19:44:21 +0200

    add icon and style for navBarFriends

---
+++ src/components/assets/svg/icons.js
@@ -0,0 +1,11 @@
+export const IconArrowUp = () =>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16"
+     fill="currentColor" className="mercado-match" width="15" height="15" focusable="false">
+    <path d="M15 11L8 6.39 1 11V8.61L8 4l7 4.61z"/>
+</svg>
+
+export const IconArrowDown = () =>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16"
+     fill="currentColor" className="mercado-match" width="15" height="15" focusable="false">
+    <path d="M1 5l7 4.61L15 5v2.39L8 12 1 7.39z"/>
+</svg>
