c78eaa8eae219199d5a23d3c41e9c68c8bc9356d
Roman Severyn <romaseverin50@gmail.com>
2022-05-23 12:52:10 +0300

    add new component AdsImage

---
+++ src/components/adsImage/AdsImage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './style.css'
+
+export const AdsImage = ({adsImage}) => {
+    return (
+        <>
+            <div>
+                <a href='#ads'>
+                    <img className='navBarAdsImg' src={adsImage} alt='ads'/>
+                </a>
+            </div>
+        </>
+    )
+}
