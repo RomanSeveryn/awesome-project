95e1b91c8685fe5e8e9fb9ca697147d3cef30d05
Roman Severyn <romaseverin50@gmail.com>
2022-05-23 12:53:41 +0300

    add new component AdsImage

---
+++ src/components/findNewFriends/FindNewFriends.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './style.css'
+
+export const FindNewFriends = ({mainInformation, search, onChangeHandler, onClick}) => {
+    return (
+        <>
+            <div className='containerAddContacts'>
+                <h5 className='addContactsHeading'>Добавить контакты</h5>
+
+                <p className='contactsParagraph'>
+                    {mainInformation}
+                    <a className='contactsParagraphLink' href=''>Подробнее</a>
+                </p>
+
+                <div>
+                    <input className='navBarInput' value={search} onChange={onChangeHandler} placeholder='test@gmail.com'/>
+                </div>
+
+                <button className='navBarButton' onClick={onClick}>Продолжить</button>
+
+                <div className='navBarLinkMore'>
+                    <a href=''>Еще</a>
+                </div>
+            </div>
+        </>
+    )
+}
